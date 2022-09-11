855d4a53582e4128bf0d7ac56fc61b74c0702412
devuday2705 <uday@qwickbit.com>
2022-09-11 19:50:51 +0530

    Navbar Added.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import { ThemeProvider } from "next-themes";
+import Script from "next/script";
+
+import { Navbar, Footer } from "../Components";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider attribute="class">
+      <div className="dark:bg-nft-dark bg-white min-h-screen">
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+      <Script
+        src="https://kit.fontawesome.com/380f1fba7a.js"
+        crossorigin="anonymous"
+      ></Script>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
